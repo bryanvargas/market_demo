98dc99350ba05f24d7b660c1b598c1af2ed282ae
bryanvargas <bryanprsvar@gmail.com>
2022-12-29 23:43:01 -0300

    first commit

---
+++ src/main/java/com/belerofonte/marketdemo/MarketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.belerofonte.marketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/belerofonte/marketdemo/MarketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belerofonte.marketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
